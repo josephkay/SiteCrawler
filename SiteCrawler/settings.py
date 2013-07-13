d07c25aa13404c939efa69fc311325d91f6f8756
josephkay <joseph.alastair@gmail.com>
2013-07-13 12:12:23 +0100

    Set up new scrapy project.
    	new file:   SiteCrawler/__init__.py
    	new file:   SiteCrawler/items.py
    	new file:   SiteCrawler/pipelines.py
    	new file:   SiteCrawler/settings.py
    	new file:   SiteCrawler/spiders/__init__.py
    	new file:   scrapy.cfg

---
+++ SiteCrawler/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for SiteCrawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'SiteCrawler'
+
+SPIDER_MODULES = ['SiteCrawler.spiders']
+NEWSPIDER_MODULE = 'SiteCrawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'SiteCrawler (+http://www.yourdomain.com)'
